7e8f94d313d2234040a365c97263d17a832371c1
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-08-30 22:42:05 -0300

    func: search book from server and display the result

--- src/NewBook.js
+++ src/NewBook.js
@@ -4,20 +4,30 @@ import './NewBook.css';
 
 class NewBook extends Component {
 
+    state = {
+        query: []
+    };
+
+    
+
     updateQuery = (inputText) => {
-        let searchResult = this.props.search(inputText, 20);
-        searchResult
-        .then(function(result){
-            result.map((item)=>(
-                console.log(item.title)
-            ))
+        const self = this;
+        this.props.search(inputText, 20)
+        .then(function(item){
+            self.setState({
+                query: item
+            })
         })
         .catch(function(error){
             console.log(error);
         });
-    }
+    };
+
+    
 
     render(){
+        let showingBooks = this.state.query;
+        console.log("ShowingBooks = " + showingBooks);
         return(
             <div>
                 <div className="search-books-bar">
@@ -25,15 +35,38 @@ class NewBook extends Component {
                         className="close-search" 
                         to="/"
                     >Back</Link>
-                    <form>
+                    
                         <input 
                             type="text"
                             onChange={(event) => this.updateQuery(event.target.value)}
                         />
-                    </form>
+                    
                 </div>
                 <div className="search-books-results">
                     
+                <ol className="books-grid">
+                        {showingBooks.map(item => (
+                            <li key={item.id}>                 
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${item.imageLinks.smallThumbnail}")` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select value={item.shelf} >
+                                                <option value="none" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{item.title}</div>
+                                    <div className="book-authors">{item.authors}</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+
                 </div>
             </div>
         );
