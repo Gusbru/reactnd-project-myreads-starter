ecc6b2cd290d7f8b848d2b4be94183cc6a5648a1
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-09-04 14:07:34 -0300

    refactor: add a book component

---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Book.css'
+
+const Book = ({currentBooks, changeStatus}) => {
+    return(
+        <ol className="books-grid">
+            {currentBooks.map(item => (
+                <li key={item.id}>                 
+                    <div className="book">
+                        <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${item.imageLinks.smallThumbnail}")` }}></div>
+                            <div className="book-shelf-changer">
+                                <select value={item.shelf} onChange={(e) => changeStatus(e.target.value, item)}>
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="book-title">{item.title}</div>
+                        <div className="book-authors">{item.authors}</div>
+                    </div>
+                </li>
+            ))}
+        </ol>
+    );
+}
+
+export default Book;
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Book from './Book';
 import './Bookshelf.css';
 
 const Bookshelf = ({ status, shelf, bookList, changeStatus }) =>  {
@@ -7,28 +8,9 @@ const Bookshelf = ({ status, shelf, bookList, changeStatus }) =>  {
         <div key={status} className="bookshelf">
             <h2 className="bookshelf-title">{shelf}</h2>
             {/* TODO: create a componente book and use here */}
-            <ol className="books-grid">
-                {currentBooks.map(item => (
-                    <li key={item.id}>                 
-                        <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${item.imageLinks.smallThumbnail}")` }}></div>
-                                <div className="book-shelf-changer">
-                                    <select value={item.shelf} onChange={(e) => changeStatus(e.target.value, item)}>
-                                        <option value="none" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="book-title">{item.title}</div>
-                            <div className="book-authors">{item.authors}</div>
-                        </div>
-                    </li>
-                ))}
-            </ol>
+            <Book 
+                currentBooks={currentBooks} 
+                changeStatus={changeStatus}/>
         </div>
     );
 }
